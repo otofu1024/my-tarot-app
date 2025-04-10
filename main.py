df3eaeb45f03b32d265e0f3d3da1c6d4dc5b2938
otofu1024 <miyamoto_eito@keio.jp>
2025-04-10 21:19:04 +0900

    全タロットカード大集合させた

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-# 基盤となるプログラム
-print("Hello, World!")
+import json
+
+cards = ["big_cards", "takara-tarot-cups", "takara-tarot-swords", "takara-tarot-wands", "takara-tarot-PENTACLES"]
+data = []
+
+for card in cards:
+    with open(f"cards_meaning/{card}.json", "r", encoding="utf-8") as f:
+        data += json.load(f)
+
+with open("cards_meaning/all_cards.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
+
+print(data)
+print(type(data))
